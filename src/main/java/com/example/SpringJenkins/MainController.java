2d6c03f6c78595a80753b2d433b41811a42418b8
naiknitesh6@gmail.com <naiknitesh6@gmail.com>
2022-07-16 11:16:28 +0530

    first commit

---
+++ src/main/java/com/example/SpringJenkins/MainController.java
@@ -0,0 +1,16 @@
+package com.example.SpringJenkins;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+public static Logger logger=LoggerFactory.getLogger(MainController.class);
+@GetMapping("/home")
+public String home()
+{logger.info("Welcome to Spring Jenkins project");
+	return "Welcome to Spring Jenkins project";
+}
+}
